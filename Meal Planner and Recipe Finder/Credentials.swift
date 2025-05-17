07186058c2239c1b95dec99596fd02e8f9064a24
A-Wattamwar <ayushwattamwarr@gmail.com>
2025-05-17 11:35:51 -0700

    corrected credentials

--- Meal Planner and Recipe Finder/Credentials.swift
+++ Meal Planner and Recipe Finder/Credentials.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-/// This file contains all the API keys and credentials for the app.
-/// Do not commit this file to source control.
+// This file contains all the API keys and credentials for the app.
+// Do not commit this file to source control.
 
 struct Credentials {
     
-    /// Edamam API Credentials
-    static let edamamAppId = "0a7189c7"
-    static let edamamAppKey = "9d11e54ebbfc928900f654db1e5f908f"
+    // Edamam API Credentials
+    static let edamamAppId = "ENTER YOUR APP ID HERE"
+    static let edamamAppKey = "ENTER YOUR APP KEY HERE"
     
-    /// Add any other API keys or credentials here
+    // Add any other API keys or credentials here
     
 }
